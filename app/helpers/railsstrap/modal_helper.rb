f84e72ccbcfb1396c58225d26f8fb2bf3df6d219
Todd Baur <todd@toadkicker.com>
2015-01-15 08:37:40 -0500

    add class option to modal so center class can be set. changed call on options to the merged var of opts

--- app/helpers/railsstrap/modal_helper.rb
+++ app/helpers/railsstrap/modal_helper.rb
@@ -4,12 +4,12 @@ module Railsstrap
     #modals have a header, a body, a footer for options.
     def modal_dialog(options = {}, &block)
       opts = default_modal_options.merge(options)
-      content_tag :div, :id => options[:id], :class => 'railsstrap-modal modal fade' do
+      content_tag :div, :id => opts[:id], :class => "railsstrap-modal modal #{opts['class']} fade" do
         content_tag :div, :class => "modal-dialog #{opts['size']}" do
           content_tag :div, :class => 'modal-content' do
-            modal_header(options[:header], &block) +
-            modal_body(options[:body], &block) +
-            modal_footer(options[:footer], &block)
+            modal_header(opts[:header], &block) +
+            modal_body(opts[:body], &block) +
+            modal_footer(opts[:footer], &block)
           end
         end
       end
@@ -47,7 +47,6 @@ module Railsstrap
 
     def modal_cancel_button(content, options)
       default_opts = { :class => 'btn railsstrap-modal-cancel-button' }
-
       content_tag_string :a, content, default_opts.merge(options)
     end
 
