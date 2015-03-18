0714860b3003bbed142de804a588b4d49c4300bc
Todd Baur <todd@toadkicker.com>
2015-03-17 19:14:55 -0700

    corrected module name

--- lib/railsstrap/helpers/date_picker_helper.rb
+++ lib/railsstrap/helpers/date_picker_helper.rb
@@ -1,41 +1,39 @@
-# Railsstrap Datepicker Helper
-#
-# Setup instructions:
-# open your application.js & application.css asset manifest file and uncomment the block to include the UI libraries
-# use the date_picker helper for the html and the date_picker_javascript helper in your templates
-#
-# You can set these options a few ways:
-# with html data-date-{OPTIONNAME},
-# or pass a data option hash into date_picker, but change the key name to_underscore format so content_tag renders the data tag properly.
-# or in date_picker_javascript, keeping it as camelCase
-#
-# pickDate: true,                 //en/disables the date picker
-# pickTime: true,                 //en/disables the time picker
-# useMinutes: true,               //en/disables the minutes picker
-# useSeconds: true,               //en/disables the seconds picker
-# useCurrent: true,               //when true, picker will set the value to the current date/time
-# minuteStepping:1,               //set the minute stepping
-# minDate:`1/1/1900`,               //set a minimum date
-# maxDate: ,     //set a maximum date (defaults to today +100 years)
-# showToday: true,                 //shows the today indicator
-# language:'en',                  //sets language locale
-# defaultDate:"",                 //sets a default date, accepts js dates, strings and moment objects
-# disabledDates:[],               //an array of dates that cannot be selected
-# enabledDates:[],                //an array of dates that can be selected
-# icons = {
-#     time: 'glyphicon glyphicon-time',
-#     date: 'glyphicon glyphicon-calendar',
-#     up:   'glyphicon glyphicon-chevron-up',
-#     down: 'glyphicon glyphicon-chevron-down'
-# }
-# useStrict: false,               //use strict when validating dates
-# sideBySide: false,              //show the date and time picker side by side
-# daysOfWeekDisabled:[]          //for example use daysOfWeekDisabled: [0,6] to disable weekends
-#
-
 module Railsstrap
-  module DatePickerHelper
-
+  module Helpers
+    # Railsstrap Datepicker Helper
+    #
+    # Setup instructions:
+    # open your application.js & application.css asset manifest file and uncomment the block to include the UI libraries
+    # use the date_picker helper for the html and the date_picker_javascript helper in your templates
+    #
+    # You can set these options a few ways:
+    # with html data-date-{OPTIONNAME},
+    # or pass a data option hash into date_picker, but change the key name to_underscore format so content_tag renders the data tag properly.
+    # or in date_picker_javascript, keeping it as camelCase
+    #
+    # pickDate: true,                 //en/disables the date picker
+    # pickTime: true,                 //en/disables the time picker
+    # useMinutes: true,               //en/disables the minutes picker
+    # useSeconds: true,               //en/disables the seconds picker
+    # useCurrent: true,               //when true, picker will set the value to the current date/time
+    # minuteStepping:1,               //set the minute stepping
+    # minDate:`1/1/1900`,               //set a minimum date
+    # maxDate: ,     //set a maximum date (defaults to today +100 years)
+    # showToday: true,                 //shows the today indicator
+    # language:'en',                  //sets language locale
+    # defaultDate:"",                 //sets a default date, accepts js dates, strings and moment objects
+    # disabledDates:[],               //an array of dates that cannot be selected
+    # enabledDates:[],                //an array of dates that can be selected
+    # icons = {
+    #     time: 'glyphicon glyphicon-time',
+    #     date: 'glyphicon glyphicon-calendar',
+    #     up:   'glyphicon glyphicon-chevron-up',
+    #     down: 'glyphicon glyphicon-chevron-down'
+    # }
+    # useStrict: false,               //use strict when validating dates
+    # sideBySide: false,              //show the date and time picker side by side
+    # daysOfWeekDisabled:[]          //for example use daysOfWeekDisabled: [0,6] to disable weekends
+    #
     def date_picker(options = {})
       opts = default_dp_options.merge(options)
       content_tag(opts[:wrapper_tag], id: opts[:id], class: opts[:wrapper_class], data: opts[:data]) {
