dcf48a01fda6ff188e06d45134536c6bdff08d20
Todd Baur <todd@toadkicker.com>
2018-01-26 21:27:29 +0900

    added support for virtually any icon library

--- lib/railsstrap/classes/icon.rb
+++ lib/railsstrap/classes/icon.rb
@@ -5,7 +5,7 @@ module Railsstrap
     # @api private
     class Icon < Base
       # @return [#to_s] the class to assign to the icon based on the Vector
-      #   Icon library used.
+      #   Icon library used. If not defined here pass through what was provided.
       def library_class
         Icon.libraries[@options[:library].to_s.underscore] || @options[:library]
       end
@@ -13,20 +13,24 @@ module Railsstrap
       # @return [#to_s] the class to assign to the icon based on the name
       #   of the icon.
       def name_class
-        if name = @options[:name]
-          "#{library_class}-#{name.to_s.gsub '_', '-'}" 
-        end
+        return if @options[:name]
+        "#{library_class}-#{name.to_s.gsub '_', '-'}"
       end
 
-    private
+
 
       # @return [Hash<Symbol, String>] the classes that Bootstrap requires to
       #   append to icons for each possible vector icon library.
+      private_class_method
       def self.libraries
         HashWithIndifferentAccess.new(nil).tap do |klass|
-          klass[:font_awesome]  = :'fa'
+          klass[:font_awesome]  = :'fas'
+          klass[:fas]  = :'fas' # font awesome solid
+          klass[:far]  = :'far' # font awesome regular
+          klass[:fal]  = :'fal' # font awesome light
+          klass[:fal]  = :'fab' # font awesome light
           klass[:glyphicons]    = :'glyphicon'
-          klass[:'']            = :'glyphicon'
+          klass[:'']            = :'far'
         end
       end
     end
